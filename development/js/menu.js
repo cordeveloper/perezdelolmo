d20085806de5ea11ea0b656df63682e98b1a462a
Cordeveloper <cordeveloper.mail@gmail.com>
2020-12-29 12:58:17 +0100

    Menu performance

--- development/js/menu.js
+++ development/js/menu.js
@@ -11,6 +11,8 @@ export default class Menu {
       this.menuBrand = this.menu.querySelector('#menu-brand');
       this.menuLogo = this.menu.querySelector('#logo span:first-child');
       this.menuLogoSub = this.menu.querySelector('#logo span:last-child');
+      this.sideMenu = document.querySelector('#side-menu');
+      this.toggleMenu = this.menu.querySelector('.menu__toggle');
       this.init = this.init.bind(this);
       this.scroll = 0;
       
@@ -21,8 +23,30 @@ export default class Menu {
         ScrollTrigger.create({
             start: 'top',
             end: 99999,
-            toggleClass: {className: 'scrolled', targets: '#menu-bar'}
+            toggleClass: {className: 'scrolled', targets: ['#menu-bar', '#side-menu',  '#menu-schedule']}
           });
+
+      this.toggleMenu.addEventListener('click', () => {
+        this.toggleMenu.classList.toggle('on');
+        this.sideMenu.classList.toggle('active');
+        if(this.sideMenu.classList.contains('active')){
+          document.querySelector('body').classList.add('block');
+        } else{
+          document.querySelector('body').classList.remove('block');
+        }
+      });
+
+      window.addEventListener('resize', () => {
+        if(this.sideMenu.classList.contains('active')){
+          this.sideMenu.classList.remove('active');
+        }
+        if(document.querySelector('body').classList.contains('block')){
+          document.querySelector('body').classList.remove('block');
+        }
+        if(this.toggleMenu.classList.contains('on')){
+          this.toggleMenu.classList.remove('on');
+        }
+      });
     }
 
 }
